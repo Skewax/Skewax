7cf579224c79e6f069053d6294fba82447e043a8
Eleanor Olson <eleanorkolson@gmail.com>
2023-10-04 14:38:33 -0400

    token parsing for requests. testing blocked by issue #19

--- backend/auth/auth.go
+++ backend/auth/auth.go
@@ -1,10 +1,14 @@
 package auth
 
 import (
+	"context"
+	"net/http"
 	"os"
+	"skewax/db"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"gorm.io/gorm"
 )
 
 type Claims struct {
@@ -12,8 +16,11 @@ type Claims struct {
 	jwt.RegisteredClaims
 }
 
+var (
+	jwtKey = os.Getenv("JWT_KEY")
+)
+
 func GenerateJWT(userId string) (string, error) {
-	key := os.Getenv("JWT_KEY")
 	claims := Claims{
 		userId,
 		jwt.RegisteredClaims{
@@ -22,10 +29,53 @@ func GenerateJWT(userId string) (string, error) {
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	ss, err := token.SignedString([]byte(key))
+	ss, err := token.SignedString([]byte(jwtKey))
 	if err != nil {
 		return "", err
 	}
 	return ss, nil
 
+}
+
+func ParseJWT(tokenStr string) (string, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		userId := claims["user_id"].(string)
+		return userId, nil
+	}
+	return "", err
+}
+
+func Middleware(orm *gorm.DB) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			header := r.Header.Get("Authorization")
+
+			// Allow unauthenticated users in
+			if header == "" {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			// Validate token
+			tokenStr := header
+			userId, err := ParseJWT(tokenStr)
+			if err != nil {
+				http.Error(w, "Invalid token", http.StatusUnauthorized)
+				return
+			}
+
+			user := orm.First(&db.AuthUser{}, "id = ?", userId)
+			if user.Error != nil {
+				http.Error(w, "Invalid token", http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), "user", &user)
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
 }
