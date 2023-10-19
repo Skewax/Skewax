c23d03bdf32fd65a81585e80d21052b04dae24cb
Eleanor Olson <eleanorkolson@gmail.com>
2023-10-19 19:46:29 -0400

    need to rebase with main

--- frontend/src/contexts/useAuth.tsx
+++ frontend/src/contexts/useAuth.tsx
@@ -1,5 +1,15 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useMemo, useState } from "react"
+import usePersistedState from "../hooks/usePersistedState";
 
+function parseJwt(token: string) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
 const gapiConfig = {
   scope: 'https://www.googleapis.com/auth/drive.file email profile openid',
   discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
@@ -18,7 +28,7 @@ interface AuthProviderProps {
 }
 
 
-const AuthContext = createContext<AuthContext>({} as any)
+const AuthContext = createContext<AuthContext>({} as AuthContext)
 
 const useAuth = () => useContext(AuthContext)
 
@@ -29,6 +39,28 @@ const gsi = window.google.accounts.oauth2.initCodeClient({
   redirect_uri: import.meta.env.VITE_LOGIN_ENDPOINT,
 })
 const AuthProvider = ({ children }: AuthProviderProps) => {
+
+  const [token, setToken] = usePersistedState<string | null>('token', null)
+  const parsed = useMemo(() => {
+    if (token != null) {
+      return parseJwt(token)
+    }
+    else {
+      return null
+    }
+  }
+    , [token])
+  console.log("parsed jwt: ", parsed)
+
+  useEffect(() => {
+    if (token !== null) {
+      const tokenOut = parseJwt(token)
+      console.log(tokenOut)
+    }
+  }, [token])
+
+
+
 
   return (
     <AuthContext.Provider value={{
