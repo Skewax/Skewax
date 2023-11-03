062595b3984486838ad68d6cda50a6beec6d527e
Eleanor Olson <eleanorkolson@gmail.com>
2023-11-03 15:50:08 -0400

    fixed checking for invalid token

--- backend/auth/auth.go
+++ backend/auth/auth.go
@@ -40,7 +40,7 @@ func GenerateJWT(userId string) (string, error) {
 
 func ParseJWT(tokenStr string) (string, error) {
 	claims := Claims{}
-	_, err := jwt.ParseWithClaims(tokenStr, &claims, func(t *jwt.Token) (interface{}, error) {
+	jwt, err := jwt.ParseWithClaims(tokenStr, &claims, func(t *jwt.Token) (interface{}, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("bad token")
 		}
@@ -48,6 +48,9 @@ func ParseJWT(tokenStr string) (string, error) {
 	})
 	if err != nil {
 		return "", err
+	}
+	if !jwt.Valid {
+		return "", fmt.Errorf("bad token")
 	}
 	return claims.UserId, nil
 }
